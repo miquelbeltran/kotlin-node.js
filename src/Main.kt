a23d4ba0c81266a49ac786b4850f97226272b167
Miquel Beltran <mike513@gmail.com>
2017-03-15 16:19:07 +0100

    simple microservice running on Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,22 @@
+@native
+fun require(module:String):dynamic
+
+@native
+var process:dynamic
+
 fun main(args: Array<String>) {
     println("Hello JavaScript!")
+
+    val express = require("express")
+    val app = express()
+
+    app.get("/", { req, res ->
+        res.type("text/plain")
+        res.send("i am a beautiful butterfly")
+    })
+
+    app.listen(3000, {
+        println("Listening on port 3000")
+    })
 }
 
