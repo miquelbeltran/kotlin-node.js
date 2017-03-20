2ee59222067921eb6cfd93fe21243c9a63e8c260
Miquel Beltran <mike513@gmail.com>
2017-03-20 19:00:25 +0100

    Amazon.de price grabber using request library

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,18 @@ fun main(args: Array<String>) {
 
     val express = require("express")
     val app = express()
+    val amazon = Amazon()
 
     app.get("/", { req, res ->
         res.type("text/plain")
         res.send("i am a beautiful butterfly")
+    })
+
+    app.get("/amazon/:asin", { req, res ->
+        res.type("text/plain")
+        amazon.getAmazonPrice(req.params.asin) {
+            res.send("Price is $it")
+        }
     })
 
     app.listen(3000, {
